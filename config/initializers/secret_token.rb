c3ecac7bd6790148920169adf5d6b582209daa4f
Brian Stearns <brianw.stearns@gmail.com>
2013-12-11 13:22:38 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wanderdesk::Application.config.secret_token = 'cc30b3848693fecda250c0a0b80e235fe0eb57bbfcf26750ff14c3d8f7ceaf1f57a07b21c93cd5116af08492fe1f3949a091c7be662af88a13ad2e2bf97dccef'
